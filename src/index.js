761b27ef7f6c6d50a87e546a1be414eb62382b6c
KR <88263939+KRofLife@users.noreply.github.com>
2021-12-03 22:13:43 +0900

    タスクのテキスト追加

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+  //  divの生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // liの生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  //  divタグの子要素に各要素を設定
+  div.appendChild(li);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
